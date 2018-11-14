747a8c5ee0131332d161fe1d8b92d3303ddee535
James Dempsey <james.dempsey420@gmail.com>
2018-11-14 11:43:41 -0600

    outline

--- src/actions/sampleaction.js
+++ src/actions/sampleaction.js
@@ -15,15 +15,14 @@ potentially dont need those
 
 update logged in user(grab profile/name, set state)
 
-save profile to DB(also set it to state)
+save profile to DB on creation(also set it to state)
 
-add trip(save to current trip state)
+add trip(save to current trips array in state)
 
 select trip(update selected trip state)
 
-select connection (set current user/profile to state)
+select connection (set connectipn user/profile to state)
 
 
 
 
- 
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -3,10 +3,15 @@
 global states needed:
 
 Logged in user(Name and Profile): 
-User Trips (City, Country, Dates):
+
+User Trips array   each trip->{
+    trips: trips array/object
+    connections: users overlap list for THIS TRIP
+
+
 Messages Data (All rows with User in user1 or user2):
+
 Potential Connections List(Names and Profiles):
-ONE SOURCE OF TRUTH
 
 Create Profile Form Data
 
