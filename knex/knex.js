a7d982cd81ab8e789276c1473a340be6b8c51efd
Abdullah Alabasi <Tech3790@gmail.com>
2018-11-15 16:11:26 -0600

    added server routes and knex queries

--- knex/knex.js
+++ knex/knex.js
@@ -1,10 +1,56 @@
-const environment = process.env.ENVIRONMENT // || 'development';
-const options = require('../knexfile.js')[environment];
-const knex = require('knex')(options);
+const environment = process.env.ENVIRONMENT; // || 'development';
+const options = require("../knexfile.js")[environment];
+const knex = require("knex")(options);
 
+getUsers = callback => {
+  knex("users").then(data => {
+    callback(data);
+  });
+};
 
-// WRITE database modules
-//    module.exports.MODELNAME
-//       knex.
+getTrips = callback => {
+  knex("trips").then(data => {
+    callback(data);
+  });
+};
 
-module.exports = knex;
+postUser = id => {
+  knex("users")
+    .insert({ auth_id: id })
+    .then(console.log("user was inserted"));
+};
+
+updateUserProfile = body => {
+  knex("users")
+    .where({ auth_id: body.auth_id })
+    .update({
+      username: body.username,
+      user_country: body.user_country,
+      picture: body.picture,
+      interests: body.inserts,
+      is_guide: body.is_guide,
+      primary_lang: body.primary_lang,
+      secondary_langs: body.secondary_langs
+    })
+    .then(console.log("user profile updated"));
+};
+
+insertTrip = body => {
+  knex("trips")
+    .insert({
+      trip_user: body.trip_user,
+      trip_city: body.trip_city,
+      trip_start: body.trip_start,
+      trip_end: body.trip_end,
+      purpose: body.purpose
+    })
+    .then(console.log("trip was inserted"));
+};
+
+module.exports = {
+  getUsers,
+  getTrips,
+  postUser,
+  updateUserProfile,
+  insertTrip
+};
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const path = require("path");
-const db = require('../knex/knex.js');
+const db = require("../knex/knex.js");
 const app = express();
 
 let port = process.env.PORT || 3000;
@@ -19,8 +19,34 @@ app.use(
   })
 );
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../dist/index.html"));
+// });
+
+// users table
+app.post("/addUser", (req, res) => {
+  db.postUser(req.body.auth_id);
+  res.sendStatus(201);
+});
+app.get("/getUsers", (req, res) => {
+  db.getUsers(data => {
+    res.send(data);
+  });
+});
+app.post("/updateUser", (req, res) => {
+  db.updateUserProfile(req.body);
+  res.sendStatus(200);
+});
+
+// trips table
+app.post("/addTrip", (req, res) => {
+  db.insertTrip(req.body);
+  res.sendStatus(201);
+});
+app.get("/getTrips", (req, res) => {
+  db.getTrips(data => {
+    res.send(data);
+  });
 });
 
 app.listen(port, function() {
