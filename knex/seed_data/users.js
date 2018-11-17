126d69b0d1dd5464d47c955ad911306dd5fa9dbc
Christopher-Shea <ctshea1@gmail.com>
2018-11-17 14:41:05 -0600

    add two user seeds for testing

--- knex/seed_data/users.js
+++ knex/seed_data/users.js
@@ -37,4 +37,24 @@ module.exports = [{
   is_guide: null,
   primary_lang: 'English',
   secondary_langs: {secondary:['Chinese']}
+},
+{
+  auth_id: '5',
+  username: null,
+  user_country: null,
+  picture: null,
+  interests: null,
+  is_guide: null,
+  primary_lang: null,
+  secondary_langs: null
+},
+{
+  auth_id: '6',
+  username: 'Person with profile and no trips',
+  user_country: 'United States',
+  picture: null,
+  interests: {hiking:false,tours:false,food:true,museums:false,landmarks:true,nightlife:true},
+  is_guide: null,
+  primary_lang: 'English',
+  secondary_langs: {secondary:['Latvian']}
 }]
