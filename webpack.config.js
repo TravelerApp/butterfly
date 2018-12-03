7bf2e1adbbcaf7cfc876ef1c48696f99f338b8d1
James Dempsey <james.dempsey420@gmail.com>
2018-12-03 12:12:42 -0600

    optimization

--- webpack.config.js
+++ webpack.config.js
@@ -18,14 +18,22 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: "image-webpack-loader",
+        // Specify enforce: 'pre' to apply the loader
+        // before url-loader/svg-url-loader
+        // and not duplicate it in rules with them
+        enforce: "pre"
       }
     ]
   },
   plugins: [htmlPlugin],
   node: {
     console: true,
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
+    fs: "empty",
+    net: "empty",
+    tls: "empty"
   }
 };
