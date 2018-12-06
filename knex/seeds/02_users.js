0a8035015151b046159b07c392ce94d50b47e547
James Dempsey <james.dempsey420@gmail.com>
2018-12-06 14:59:36 -0600

    wheeee

--- knex/seeds/02_users.js
+++ knex/seeds/02_users.js
@@ -1,8 +1,9 @@
-const usersData = require('../seed_data/users.js');
+const usersData = require("../seed_data/Abdullah_users.js");
 
 exports.seed = function(knex, Promise) {
-  return knex('users').del()
-    .then(function () {
-      return knex('users').insert(usersData);
+  return knex("users")
+    .del()
+    .then(function() {
+      return knex("users").insert(usersData);
     });
 };
--- knex/seeds/03_trips.js
+++ knex/seeds/03_trips.js
@@ -1,8 +1,9 @@
-const tripsData = require('../seed_data/trips.js');
+const tripsData = require("../seed_data/Abdullah_trips.js");
 
 exports.seed = function(knex, Promise) {
-  return knex('trips').del()
-    .then(function () {
-      return knex('trips').insert(tripsData);
+  return knex("trips")
+    .del()
+    .then(function() {
+      return knex("trips").insert(tripsData);
     });
 };
